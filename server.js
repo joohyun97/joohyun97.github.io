bb8978e8cebbddeb4406a5c601dfbb2dd9ac766b
joohyun97 <jpark97@vt.edu>
2024-04-10 20:37:09 -0400

    add test at package.json

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/api/books/details/:bookID', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
